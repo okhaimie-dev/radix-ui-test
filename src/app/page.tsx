ea4fec0ecfadef8cde265ba53a3fcd8f9af97f29
okhaimie-dev <oktonystark8@gmail.com>
2023-07-01 23:20:36 -0500

    radix makes more sense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,19 @@ import './styles.css'
 
 import * as Popover from "@radix-ui/react-popover";
 
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Popover.Root>
-				<Popover.Trigger>More info</Popover.Trigger>
-				<Popover.Portal>
-					<Popover.Content>
-						Some more info…
-						<Popover.Arrow />
-					</Popover.Content>
-				</Popover.Portal>
-			</Popover.Root>
-		</main>
-	);
-}
+const PopoverDemo = () => (
+	<Popover.Root>
+		<Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
+		<Popover.Portal>
+			<Popover.Content
+				className="PopoverContent"
+				sideOffset={5}
+			>
+				Some more info…
+				<Popover.Arrow className="PopoverArrow" />
+			</Popover.Content>
+		</Popover.Portal>
+	</Popover.Root>
+);
+
+export default PopoverDemo;
